68bc1d151cd6867373001ec47238ffd630fb431b
HyunJu1 <hh960305@naver.com>
2018-05-30 12:13:26 +0900

    add 고객 삭제

--- routes/project.js
+++ routes/project.js
@@ -33,6 +33,22 @@ var isAuthenticated2 = function (req, res, next) {
   res.redirect('/signin');
  
 };
+
+
+var isAuthenticated3 = function (req, res, next) {
+  
+  if (req.isAuthenticated()){
+    if(req.user.dept ==3){
+      //경영진만 가능
+      return next();
+    }
+    req.flash('danger','접근 권한이 없습니다.');
+    res.redirect('/project/order_list');
+    
+  }
+  res.redirect('/signin');
+ 
+};
 router.get('/', isAuthenticated, function (req, res,next) {
   connection.query(' select * from project, cus_order, customer where project.pro_org=cus_order.order_id and cus_order.cus_id=customer.cus_id',
   function(err,projects){
@@ -93,7 +109,7 @@ router.get('/add_project', isAuthenticated2, function (req, res,next) {
   });
 });
 
-router.get('/add_order', isAuthenticated2, function (req, res,next) {
+router.get('/add_order', isAuthenticated3, function (req, res,next) {
   connection.query('select * from customer',
   function(err,clients){
     if(err) throw err;
@@ -103,7 +119,7 @@ router.get('/add_order', isAuthenticated2, function (req, res,next) {
   });
 });
 
-router.get('/:id/edit_order', isAuthenticated2, function (req, res,next) {
+router.get('/:id/edit_order', isAuthenticated3, function (req, res,next) {
   connection.query('select * from cus_order where order_id=?',req.params.id,
   function(err,orders){
     if(err) throw err;
@@ -113,7 +129,7 @@ router.get('/:id/edit_order', isAuthenticated2, function (req, res,next) {
       });
   });
 });
-router.post('/:id/edit_order', isAuthenticated2, (req, res, next) => {
+router.post('/:id/edit_order', isAuthenticated3, (req, res, next) => {
 
 
   connection.query('UPDATE cus_order SET  order_content=? ,order_date=? WHERE order_id=? ',
@@ -143,7 +159,7 @@ router.post('/:id/answers', isAuthenticated2, (req, res, next) => {
   });
 });
 
-router.post('/add_order', isAuthenticated2, (req, res, next) => {
+router.post('/add_order', isAuthenticated3, (req, res, next) => {
   if(req.body.client_add){
     connection.query('insert into customer(cus_name) values(?)',
     req.body.client_add, function(err,result){
@@ -287,7 +303,7 @@ router.get('/:id/delete', isAuthenticated2, (req, res, next) => {
   });
 });
 
-router.get('/:id/delete_order', isAuthenticated2, (req, res, next) => {
+router.get('/:id/delete_order', isAuthenticated3, (req, res, next) => {
   connection.query('DELETE FROM cus_order WHERE order_id = ?',req.params.id,function(err,result){
     if (err) {
       return next(err);
@@ -303,6 +319,15 @@ router.get('/:id/delete_answer', isAuthenticated2, (req, res, next) => {
     }
     req.flash('success', 'Deleted Successfully.');
     res.redirect('/project');
+  });
+});
+router.get('/:id/delete_customer', isAuthenticated3, (req, res, next) => {
+  connection.query('DELETE FROM customer WHERE cus_id = ?',req.params.id,function(err,result){
+    if (err) {
+      return next(err);
+    }
+    req.flash('success', 'Deleted Successfully.');
+    res.redirect('/project/order_list');
   });
 });
 
